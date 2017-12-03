fb30714223486c526288b2dc5d73436313cc406a
estesolutions <jason@estesolutions.com>
2017-12-02 22:23:45 -0500

    Adding source files

---
+++ README.md
@@ -0,0 +1,1 @@
+"# tutorial--globalfunctions-mule" 
---
+++ src/main/resources/com/jasonestevan/entities/Person.java
@@ -0,0 +1,29 @@
+package com.jasonestevan.entities;
+
+public class Person {
+
+	private String name;
+	private double itemsTotal;
+	private float orderTotal;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getItemsTotal() {
+		return itemsTotal;
+	}
+	public void setItemsTotal(double itemsTotal) {
+		this.itemsTotal = itemsTotal;
+	}
+	public float getOrderTotal() {
+		return orderTotal;
+	}
+	public void setOrderTotal(float orderTotal) {
+		this.orderTotal = orderTotal;
+	}
+
+}
